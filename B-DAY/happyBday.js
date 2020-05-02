715dc07d4383b5d6248f1447dfd0da0d0e84ad70
Alpacha <pahomovvladimir1@gmail.com>
2020-05-02 14:14:29 -0700

    adding b-day

---
+++ B-DAY/happyBday.js
@@ -0,0 +1,9 @@
+// function moveBtn() {
+//     var move = document.getElementById("btn");
+//     move.style.left = move.offsetLeft + 100 + 'px';
+
+// }
+
+function showImage(){
+    document.getElementById('loadingImage').style.visibility="visible";
+}
